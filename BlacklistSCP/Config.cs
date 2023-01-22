beff4f8c2b90725ce83e2291eb6dc14b20d4ec32
VALERA771 <72030575+VALERA771@users.noreply.github.com>
2023-01-22 20:30:57 +0300

    Add files via upload

---
+++ BlacklistSCP/Config.cs
@@ -0,0 +1,35 @@
+﻿using Exiled.API.Interfaces;
+using PlayerRoles;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlacklistSCP
+{
+    public sealed class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+
+        [Description("List of blacklisted roles")]
+        public List<RoleTypeId> BlacklistedRoles { get; set; } = new List<RoleTypeId>()
+        {
+            RoleTypeId.Scp049,
+            RoleTypeId.Scp173,
+            RoleTypeId.Scp096,
+            RoleTypeId.Scp079,
+            RoleTypeId.Scp939,
+            RoleTypeId.Scp106
+        };
+
+        [Description("List or roles for which players with blaclisted role can be swaped")]
+        public List<RoleTypeId> SwapRoles { get; set; } = new List<RoleTypeId>()
+        {
+            RoleTypeId.ClassD,
+            RoleTypeId.Spectator
+        };
+    }
+}
---
+++ BlacklistSCP/Plugin.cs
@@ -0,0 +1,62 @@
+﻿using Exiled.API.Features;
+using MEC;
+using System;
+
+using Server = Exiled.Events.Handlers.Server;
+
+namespace BlacklistSCP
+{
+    public class Plugin : Plugin<Config>
+    {
+        public Plugin() 
+        { 
+        }
+
+        public Plugin Instance => _singleton;
+        private Plugin _singleton;
+
+        public override string Name => base.Name;
+        public override string Prefix => base.Prefix;
+        public override string Author => base.Author;
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(6, 0, 0);
+
+        public override void OnEnabled()
+        {
+            Server.RoundStarted += OnRoundStarted;
+
+            _singleton = this;
+
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled() 
+        {
+            Server.RoundStarted -= OnRoundStarted;
+
+            _singleton = null;
+
+            base.OnDisabled();
+        }
+
+        public override void OnReloaded()
+        {
+            base.OnReloaded();
+        }
+
+
+        void OnRoundStarted()
+        {
+            Timing.CallDelayed(0.5f, () =>
+            {
+                foreach (Player player in Player.List)
+                {
+                    if (Instance.Config.BlacklistedRoles.Contains(player.Role.Type))
+                    {
+                        player.Role.Set(Instance.Config.SwapRoles.RandomItem());
+                    }
+                }
+            });
+        }
+    }
+}
